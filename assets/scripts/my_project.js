70bcccc231b330c6e4ef7ac7fc195fb0b5891ed4
GirindraSW <girindrasulistiyowardoyo@gmail.com>
2026-01-30 20:53:52 +0700

    update: renderelement in my_project.js

--- assets/scripts/my_project.js
+++ assets/scripts/my_project.js
@@ -19,8 +19,46 @@ biodata.addEventListener("submit", function (e){
     console.log(users);
 
     changeElement();
+    renderUsers();
 })
 
+// notif/header about project  submited
 function changeElement(){
-    document.getElementById
+    document.getElementById("header-user").innerHTML = 
+    `<p> project details submited</p>`
 }
+
+function renderUsers() {
+    userList.innerHTML = "";
+
+    for (let i = 0; i < users.length; i++) {
+        userList.innerHTML += `
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            ${users[i].firstName} ${users[i].lastName}
+                        </h5>
+                        <p class="card-text text-muted">
+                            Project submitted
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+// function renderUsers(){
+//     userList.innerHTML = "";
+
+//     for (let i = 0; i < users.length; i++){
+//         userList.innerHTML += `
+//          <div>
+//         <h5>${users[i].firstName}</h5>
+//         <h5>${users[i].lastName}</h5>
+//         </div>
+//         `
+        
+//     }
+// }
